0e01dc0e040e88cabba5ef7a897b96a4803ff456
oriharaa <qstar9537@naver.com>
2021-03-28 03:27:36 +0900

    Up to the footer area

--- main.js
+++ main.js
@@ -1,18 +1,69 @@
 'use strict';
 
 const viewfortUl = document.querySelector('.slider-viewfort ul');
-const maxWidth = viewfortUl.clientWidth*-1;
-let moveInterval = -1080;
 
-let i = 1;
+const firstMaxWidth = viewfortUl.clientWidth*-1; //ul너비 11,880
+const secondMaxWidth = viewfortUl.clientWidth*-1; //ul너비 7,590
+const thirdMaxWidth = viewfortUl.clientWidth*-1; //ul너비 3,740
 
-function autoMoveSlider(number){
-  let width3d = number*i;
-  if(width3d === maxWidth){
-    i = 0;    
+
+const firstQuarter = -1080; //슬라이더 width 960px + margin 120px 더한 값의 음수
+const secondQuarter = -690; //width 670px + margin 20px
+const thirdQuarter = -340; //width 335px + margin 5px
+
+let i = 0;
+
+function autoMoveSlider(){
+
+  if(window.innerWidth > 1000){
+    console.log("first");
+    firstAutoMoveSlider(firstQuarter);
+  }
+  else if(680 < window.innerWidth && window.innerWidth <= 1000){
+    console.log("second");
+    secondAutoMoveSlider(secondQuarter);
+  }
+  else if(window.innerWidth <= 680){
+    console.log("third");
+    thirdAutoMoveSlider(thirdQuarter);
+  }
+};
+
+const timer = setInterval(autoMoveSlider, 5000);
+// clearInterval(timer);
+
+
+function secondAutoMoveSlider(number){
+  let secondWidth3d = number*i;
+        console.log(`i = ${i}`);
+        console.log(`number = ${number}`);
+        console.log(`secondWidth3d = ${secondWidth3d}`);
+        console.log(`secondMaxWidth = ${secondMaxWidth}`);
+  if(secondWidth3d <= secondMaxWidth){
+    i=0;
   }
   viewfortUl.style.transform = `translate3d(${number*i}px, 0px, 0px)`;
   i++;
-};
+}
 
-const timer = setInterval(autoMoveSlider, 5000, moveInterval);
+function firstAutoMoveSlider(number){
+  let firstWidth3d = number*i;
+  if(firstWidth3d <= firstMaxWidth){
+    i=0;
+  }
+  viewfortUl.style.transform = `translate3d(${number*i}px, 0px, 0px)`;
+  i++;
+}
+
+function thirdAutoMoveSlider(number){
+  let thirdWidth3d = number*i;
+        console.log(`i = ${i}`);
+        console.log(`number = ${number}`);
+        console.log(`thirdWidth3d = ${thirdWidth3d}`);
+        console.log(`thirdMaxWidth = ${thirdMaxWidth}`);
+  if(thirdWidth3d <= thirdMaxWidth){
+    i=0;
+  }
+  viewfortUl.style.transform = `translate3d(${number*i}px, 0px, 0px)`;
+  i++;
+}
