c98826a39c4a2ce39fc721f5e28e5bff017255af
oriharaa <qstar9537@naver.com>
2021-03-29 04:24:08 +0900

    Remaining slider part

--- header.js
+++ header.js
@@ -1,1 +1,64 @@
 'use strict';
+
+const toggleBtn = document.querySelector('#header .toggle-btn');
+
+const headerLogo = document.querySelector('#header .header__logo a img');
+const sadan = document.querySelector('#header .sadan');
+const lang = document.querySelector('#header .lang');
+
+const activeNav = document.querySelector('#header .active');
+const basicNav = document.querySelector('#header .navbar__wrap');
+const searchBtn = document.querySelector('#header .until-search');
+
+const searchPopUp = document.querySelector('#searchPop-up');
+const xBtn = document.querySelector('#searchPop-up .btn-close');
+
+
+
+toggleBtn.addEventListener('click', ()=>{
+  showActiveNav();
+  hideBasicNav();
+  changeBtn();
+  changeLogo();
+  changeSadan();
+});
+
+searchBtn.addEventListener('click',()=> showPopUp());
+xBtn.addEventListener('click', ()=> hidePopUp());
+
+
+function showActiveNav(){
+  activeNav.classList.toggle('hide');
+};
+
+function hideBasicNav(){
+  basicNav.classList.toggle('hide');
+}
+
+function changeBtn(){
+  toggleBtn.classList.toggle('x-btn');
+};
+
+function changeLogo(){
+  headerLogo.classList.toggle('change');
+  const hasClass = headerLogo.classList.contains('change');
+  if(hasClass){
+    headerLogo.setAttribute('src', "./images/header/logo_02.png");
+  }
+  else{
+    headerLogo.setAttribute('src', "./images/header/logo_01.png");
+  }
+};
+
+function changeSadan(){
+  sadan.classList.toggle('change');
+  lang.classList.toggle('fixed');
+};
+
+function showPopUp(){
+  searchPopUp.classList.toggle('hide');
+};
+
+function hidePopUp(){
+  searchPopUp.classList.toggle('hide');
+}
--- swiper.js
+++ swiper.js
@@ -8,33 +8,25 @@ const thirdQuarter = -340; //width 335px + margin 5px
 
 let i = 0;
 
+const timer = setInterval(autoMoveSlider, 5000);
+// clearInterval(timer);
+
 function autoMoveSlider(){
 
   if(window.innerWidth > 1000){
-    console.log("first");
     firstAutoMoveSlider(firstQuarter);
   }
   else if(680 < window.innerWidth && window.innerWidth <= 1000){
-    console.log("second");
     secondAutoMoveSlider(secondQuarter);
   }
   else if(window.innerWidth <= 680){
-    console.log("third");
     thirdAutoMoveSlider(thirdQuarter);
   }
 };
 
-const timer = setInterval(autoMoveSlider, 5000);
-// clearInterval(timer);
-
-
 function secondAutoMoveSlider(number){
   const secondMaxWidth = viewfortUl.clientWidth*-1; //ul너비 7,590
   let secondWidth3d = number*i;
-        console.log(`i = ${i}`);
-        console.log(`number = ${number}`);
-        console.log(`secondWidth3d = ${secondWidth3d}`);
-        console.log(`secondMaxWidth = ${secondMaxWidth}`);
   if(secondWidth3d <= secondMaxWidth){
     i=0;
   }
@@ -55,10 +47,6 @@ function firstAutoMoveSlider(number){
 function thirdAutoMoveSlider(number){
   const thirdMaxWidth = viewfortUl.clientWidth*-1; //ul너비 3,740
   let thirdWidth3d = number*i;
-        console.log(`i = ${i}`);
-        console.log(`number = ${number}`);
-        console.log(`thirdWidth3d = ${thirdWidth3d}`);
-        console.log(`thirdMaxWidth = ${thirdMaxWidth}`);
   if(thirdWidth3d <= thirdMaxWidth){
     i=0;
   }
